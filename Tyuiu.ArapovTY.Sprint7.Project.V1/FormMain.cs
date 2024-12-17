afe623ec21e3061b18b020e366e95b4c07376e2e
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-12-17 20:13:37 +0500

    Project

--- Tyuiu.ArapovTY.Sprint7.Project.V1/FormMain.cs
+++ Tyuiu.ArapovTY.Sprint7.Project.V1/FormMain.cs
@@ -1,4 +1,7 @@
+using System.IO;
+using System.Windows.Forms;
 using Tyuiu.ArapovTY.Sprint7.Project.V1.Lib;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Tyuiu.ArapovTY.Sprint7.Project.V1
 {
     public partial class FormMainLoad_ATY : Form
@@ -6,12 +9,32 @@ namespace Tyuiu.ArapovTY.Sprint7.Project.V1
         public FormMainLoad_ATY()
         {
             InitializeComponent();
+            openFileDialogProject_ATY.Filter = "Значения, разделённые запятыми(*.csv)|*.csv|Все файлы(*.*)|*.*";
+            saveFileDialogProject_ATY.Filter = "Значения, разделённые запятыми(*.csv)|*.csv|Все файлы(*.*)|*.*";
         }
         DataService ds = new DataService();
         static int rows;
         static int columns;
         static string openFilePath;
 
+        public int[,] LoadFromFileData(string filePath)
+        {
+            string fileData = File.ReadAllText(filePath);
+            fileData = fileData.Replace("\n", "\r");
+            string[] lines = fileData.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            int rows = lines.Length;
+            int columns = lines[0].Split(';').Length;
+            int[,] arrayValues = new int[rows, columns];
+            for (int r = 0; r < rows; r++)
+            {
+                string[] line_r = lines[r].Split(';');
+                for (int c = 0; c < columns; c++)
+                {
+                    arrayValues[r, c] = Convert.ToInt32(line_r[c]);
+                }
+            }
+            return arrayValues;
+        }
         private void buttonHelp_ATY_Click(object sender, EventArgs e)
         {
             FormAbout_ATY formAbout = new FormAbout_ATY();
@@ -21,6 +44,6 @@ namespace Tyuiu.ArapovTY.Sprint7.Project.V1
         private void buttonHelp_ATY_MouseEnter(object sender, EventArgs e)
         {
             toolTipProject_ATY.ToolTipTitle = "Справка";
-        }
+        } 
     }
 }
